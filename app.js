8c12e8f642a34b4d02f6393f1a4ba0cef3eabf27
Emilie Brohet <emilie.brohet@gmail.com>
2024-05-16 14:08:35 +0200

    Ajout des interactions avec la base de données

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Book = require('./models/book');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://emiliebrohet:v5fPe8zc7lkf8y3M@cluster0.ldeyi4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
@@ -12,46 +21,37 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/books', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Livre créé !'
-    });
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
+  });
+  book.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
 });
 
 app.get('/api/books', (req, res, next) => {
-    const books = [
-        {
-            userId: 'qsomihvqios',
-            title: 'Mon premier livre',
-            author: 'Bernard Werber',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            year: 2000,
-            genre: 'science-fiction',
-            ratings: [
-                {
-                    userId: 'qsomihvqios',
-                    grade: 4
-                }
-            ],
-            averageRating: 3
-          },
-          {
-            userId: 'qsomihvqios',
-            title: 'Mon deuxième livre',
-            author: 'Agatha Christie',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            year: 1940,
-            genre: 'policier',
-            ratings: [
-                {
-                    userId: 'qsomihvqios',
-                    grade: 3
-                }
-            ],
-            averageRating: 3
-          },
-    ];
-    res.status(200).json(books);
+  Book.find()
+  .then(books => res.status(200).json(books))
+  .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
