3c17bf40b72db7693a3ae9c70d52f0911de44e9d
Emilie Brohet <emilie.brohet@gmail.com>
2024-05-21 14:58:00 +0200

    Ajout de la gestion de fichiers avec multer

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+const multer = require ('../middleware/multer-config');
+
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', auth, stuffCtrl.createBook);
-router.put('/:id', auth, stuffCtrl.modifyBook);
+router.post('/', auth, multer, stuffCtrl.createBook);
+router.put('/:id', auth, multer, stuffCtrl.modifyBook);
 router.delete('/:id', auth, stuffCtrl.deleteBook);
-router.get('/:id', auth, stuffCtrl.getOneBook);
-router.get('/', auth, stuffCtrl.getAllBooks);
+router.get('/:id', stuffCtrl.getOneBook);
+router.get('/', stuffCtrl.getAllBooks);
 
 module.exports = router;
